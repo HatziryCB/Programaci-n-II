a264834f92f95f599c377c5329a0259d46f7cee9
Hatziry Nayaly Chacon Berreondo <hchaconb@miumg.edu.gt>
2022-08-05 00:39:09 -0600

    Commit #3: Realizando estructura del servlet para mostrar los datos del usuario, implementando estilos de bootstrap, haciendo indentaciones

--- Hatziry_09092114813/src/java/ClienteController.java
+++ Hatziry_09092114813/src/java/ClienteController.java
@@ -3,18 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import PaqueteJava.ClienteModell;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Chacón Berreondo
  */
+@WebServlet(urlPatterns = {"/ClienteController"})
 public class ClienteController extends HttpServlet {
+
+    ClienteModell cliente;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,14 +34,66 @@ public class ClienteController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ClienteController</title>");            
+            out.println("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct\" crossorigin=\"anonymous\"></script>"
+                    + "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css\" integrity=\"sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N\" crossorigin=\"anonymous\">\n");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ClienteController at " + request.getContextPath() + "</h1>");
+            out.println("<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n"
+                    + "            <a class=\"navbar-brand\" href=\"#\">UMG</a>\n"
+                    + "            <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n"
+                    + "              <span class=\"navbar-toggler-icon\"></span>\n"
+                    + "            </button>\n"
+                    + "\n"
+                    + "            <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">\n"
+                    + "              <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">\n"
+                    + "                <li class=\"nav-item active\">\n"
+                    + "                  <a class=\"nav-link\" href=\"#\">Registrarse <span class=\"sr-only\">(current)</span></a>\n"
+                    + "                </li>\n"
+                    + "                <li class=\"nav-item\">\n"
+                    + "                  <a class=\"nav-link\" href=\"#\">Tienda</a>\n"
+                    + "                </li>\n"
+                    + "                <li class=\"nav-item\">\n"
+                    + "                  <a class=\"nav-link disabled\">Acerca de nosotros</a>\n"
+                    + "                </li>\n"
+                    + "              </ul>\n"
+                    + "              <form class=\"form-inline my-2 my-lg-0\">\n"
+                    + "                <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\">\n"
+                    + "                <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Buscar</button>\n"
+                    + "              </form>\n"
+                    + "            </div>\n"
+                    + "        </nav>");
+            out.print("<div class=\"mx-auto\" style=\"width: 600px;\">\n"
+                    + "<h1 class='display-4'>Datos de Usuario</h1>"
+                    + "<br><br>"
+                    + "<dl class=\"row\">\n"
+                    + "  <dt class=\"col-sm-3\">Description lists</dt>\n"
+                    + "  <dd class=\"col-sm-9\">A description list is perfect for defining terms.</dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3\">Term</dt>\n"
+                    + "  <dd class=\"col-sm-9\">\n"
+                    + "    <p>Definition for the term.</p>\n"
+                    + "    <p>And some more placeholder definition text.</p>\n"
+                    + "  </dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3\">Another term</dt>\n"
+                    + "  <dd class=\"col-sm-9\">This definition is short, so no extra paragraphs or anything.</dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3 text-truncate\">Truncated term is truncated</dt>\n"
+                    + "  <dd class=\"col-sm-9\">This can be useful when space is tight. Adds an ellipsis at the end.</dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3\">Nesting</dt>\n"
+                    + "  <dd class=\"col-sm-9\">\n"
+                    + "    <dl class=\"row\">\n"
+                    + "      <dt class=\"col-sm-4\">Nested definition list</dt>\n"
+                    + "      <dd class=\"col-sm-8\">I heard you like definition lists. Let me put a definition list inside your definition list.</dd>\n"
+                    + "    </dl>\n"
+                    + "  </dd>\n"
+                    + "</dl>"
+                    + "</div>");
             out.println("</body>");
             out.println("</html>");
         }
---
+++ Hatziry_09092114813/src/java/PaqueteJava/ClienteModell.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PaqueteJava;
+
+/**
+ *
+ * @author Chacón Berreondo
+ */
+public class ClienteModell {
+    private String nombre;
+    private String apellido;
+    private int codigo;
+    private int telefono;
+    private String correo;
+    private String pass;
+    private String dirección;
+    private String municipio;
+    private String ciudad;
+    
+    public ClienteModell(){
+        this.nombre="Nombre de ejemplo";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    
+}
